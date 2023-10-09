ecc59506aef6b4a425d2f3cf3f3c858997c883dd
Julian Sarmiento <julian.sarmiento.jeso.com>
2023-10-09 17:15:05 -0500

    Plus ejercicio 1

--- Ejercicio 1/script.js
+++ Ejercicio 1/script.js
@@ -1,15 +1,17 @@
-var num1 = parseInt(prompt("digite un numero",0));
-var num2 = parseInt(prompt("digite otro numero",0));
+var num1 = parseInt(prompt("digite un numero", 0));
+var num2 = parseInt(prompt("digite otro numero", 0));
+
+/*while (num1 <= 0 || num2 <= 0 || isNaN(num1) || isNaN(num2)) {
+    num1 = parseInt(prompt("digite un numero", 0));
+    num2 = parseInt(prompt("digite otro numero", 0));
+}*/
 
 if (num1 > num2) {
-   alert("El numero mayor es " + num1 + " el numero menor es " + num2)
-}
-else if (num1 < num2) {
-   alert("El numero mayor es " + num2 + " el numero menor es " + num1)
-}
-else if (num1 == num2){
-    alert("Los numeros son iguales")
-}
-else {
-    alert("Introduzca números correctos")
+  alert("El numero mayor es " + num1 + " el numero menor es " + num2);
+} else if (num1 < num2) {
+  alert("El numero mayor es " + num2 + " el numero menor es " + num1);
+} else if (num1 == num2) {
+  alert("Los numeros son iguales");
+} else {
+  alert("Introduzca números correctos");
 }
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Ejercicio 1 : 
 Hacer un programa que nos pida 2 numeros y que diga cual es mayor, cual es menor y si son iguales
+- Plus: Si los numeros no son un numero o son menores o iguales a 0, los vuelva a pedir (Comentareado)
